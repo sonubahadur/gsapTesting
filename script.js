6539c648cec86ba158c11de0df0d0d2676ede68c
SONU Bahadur (Sorav) <soravsharma3210@gmail.com>
2025-01-24 15:55:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline()
+
+tl.from("h2",{
+    y:-20,
+    opacity:0,
+    duration:0.5,
+    delay:0.3
+})
+tl.from("h4",{
+    y:-20,
+    opacity:0,
+    duration:0.5,
+    delay:0.5,
+    stagger:0.3
+})
+tl.from("h1",{
+    y:20,
+    opacity:0,
+    duration:1,
+    scale:0.2
+})
